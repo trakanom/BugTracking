bc471ce6ea2b12f42b5273d416c0188c24f25af8
Trakanom <Mika.Wisener@gmail.com>
2022-07-05 16:45:09 -0700

    populate the Project_Window with fields & buttons

--- resources/square_things/Edit_Project.py
+++ resources/square_things/Edit_Project.py
@@ -12,9 +12,48 @@ class Project_Window:
         #When adding a project, prefill all fields with basic info.
         pass
     def Open_Window(self):
-        pass
         #Initialize window
     
+        self.root = Tk()
+        self.root.title("Edit Project")
+        
+        self.author_label = Label(self.root,text="Author:").grid(column=0,row=0,columnspan=1)
+        self.author = Entry(self.root, width=30)
+        self.author.grid(column=1, row=0, columnspan=1)
+        
+        self.date_label = Label(self.root,text="Date Submitted:").grid(column=2,row=0,columnspan=1)
+        self.date_submitted = Entry(self.root)
+        self.date_submitted.grid(column=3, row=0, columnspan=1)
+        
+        self.urgency_label = Label(self.root,text="Urgency:").grid(column=4,row=0,columnspan=1)
+        self.urgency = Entry(self.root, width=30)
+        self.urgency.grid(column=5, row=0, columnspan=1)
+        
+        self.title = Entry(self.root, width=75)
+        self.title.grid(column=1, row=1, columnspan=3)
+        self.title_label = Label(self.root,text="Title:").grid(column=0,row=1,columnspan=1)
+        
+        self.description_label = Label(self.root,text="Description:").grid(column=0,row=2,columnspan=1)
+        self.description = Entry(self.root, width=75)
+        self.description.grid(column=1, row=2, columnspan=3)
+        
+        self.due_date_label = Label(self.root,text="Due Date:").grid(column=4,row=1,columnspan=1)
+        self.date_due = Entry(self.root, width=30)
+        self.date_due.grid(column=5, row=1, columnspan=1)
+        
+        self.assignee_label = Label(self.root,text="Assignee:").grid(column=4,row=2,columnspan=1)
+        self.assignee = Entry(self.root, width=30)
+        self.assignee.grid(column=5, row=2, columnspan=1)
+        
+    
+        
+        
+        
+        Button(self.root, text="Add Attachments", command=None).grid(column=3, row=10, columnspan=2)
+        Button(self.root, text="Save", command=self.Save_Project).grid(column=4, row=11, columnspan=2)
+        Button(self.root, text="Cancel", command=self.Close_Window).grid(column=5, row=11, columnspan=2)
+        # Button(self.root, text="Delete Attachment", command=None).grid(column=0, row=5, columnspan=2)
+        self.root.mainloop()
     def Close_Window(self):
         self.root.destroy()
     def Save_Project(self):
