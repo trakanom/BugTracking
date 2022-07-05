12abfb80abdfd16f6b4dc9da192879ebd0612911
Trakanom <Mika.Wisener@gmail.com>
2022-07-05 16:55:16 -0700

    allow the creation of labelled entry fields easily

--- resources/classes/Windows.py
+++ resources/classes/Windows.py
@@ -8,8 +8,14 @@ class Window:
         self.labels={}
         self.buttons={}
     def Add_Field(self,title,column,row,width=None,height=None,columnspan=None,rowspan=None):
-        pass
         #Let's make some fields for Entries
+        columnspan=1 if columnspan == None else columnspan
+        rowspan=1 if rowspan == None else rowspan
+        width=30 if width == None else width
+        height=8 if columnspan == None else columnspan
+        self.fields[title]=Entry(self.root)
+        self.labels[title]=Label(self.root,text=title).grid(column=column,row=row)
+        self.fields[title].grid(column=(column+1),row=row,columnspan=columnspan,rowspan=rowspan)
     def Create_Window(self):
         #When you're done building the window, use this.
         self.root.mainloop()
