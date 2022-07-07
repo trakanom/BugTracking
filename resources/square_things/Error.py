04be416cc49b51d6b348ac54728a9e1786343f83
Trakanom <Mika.Wisener@gmail.com>
2022-07-06 23:10:02 -0700

    make the error window sleeker

--- resources/square_things/Error.py
+++ resources/square_things/Error.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from ..config import errors
+from ..classes.Windows import Window
 class ErrorPage:
     def __init__(self, errortype):
         #Error Window with appropriate message
+        self.root = Tk()
+        text = Label(self.root,text=errors[errortype]).grid(column=0,row=0,columnspan=3)
+        # text.insert(INSERT,errors[errortype])
+        # text.pack()
+        # print(errors[errortype])
+        button = Button(self.root,text="Okay",command=self.confirm).grid(column=1,row=1)
+        # button.pack()
+    def confirm(self):
+        #Button to close window
+        self.root.destroy()
+    def Old_Init(self,errortype):
+    #Error Window with appropriate message
         self.root = Tk()
         text = Text(self.root)
         text.insert(INSERT,errors[errortype])
@@ -10,6 +23,3 @@ class ErrorPage:
         # print(errors[errortype])
         button = Button(self.root,text="Okay",command=self.confirm)
         button.pack()
-    def confirm(self):
-        #Button to close window
-        self.root.destroy()
