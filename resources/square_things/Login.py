429b062a4481c08f5526c05376b2e0c4123e4fe3
Trakanom <Mika.Wisener@gmail.com>
2022-07-07 15:36:54 -0700

    refactor Login for new framework

--- resources/square_things/Login.py
+++ resources/square_things/Login.py
@@ -19,24 +19,21 @@ class Login:
             Landing = Landing_Page(self.Userlist,user)
             Landing.Open_Window()
         else:
-            self.Login_Error("Incorrect Information")
+            ErrorPage("Incorrect Information")
         # except:
-            # self.Login_Error("Missing Field")
+            # ErrorPage("Missing Field")
     def Edit_Users(self):
-        try:
-            user = self.root.fields["Username"].get()
-            if self.Userlist.TheList[user].PW_Hash == self.root.fields["Password"].get():
-                from .Edit_User import Edit_Users
-                print("We're in.")
-                self.root.Close_Window()
-                EditUsers = Edit_Users(self.Userlist,user)
-                EditUsers.Open_Window()
-            else:
-                self.Login_Error("Incorrect Information")
-        except AttributeError:
-            self.Login_Error("Missing Field")
-        except:
-            self.Login_Error("Another Error")
+        
+        user = self.root.fields["Username"].get()
+        #replace this with auth stuff
+        if (user in self.Userlist.TheList.keys()) and (self.Userlist.TheList[user].PW_Hash == self.root.fields["Password"].get()):
+            from .Edit_User import Edit_Users
+            print("We're in.")
+            self.root.Close_Window()
+            EditUsers = Edit_Users(self.Userlist,user)
+            EditUsers.Open_Window()
+        else:
+            ErrorPage("Incorrect Information")
     def Open_Window(self):
         #Initialize window with fields for username & password, and buttons for logging in and editing users
         self.root = Tk()
@@ -50,11 +47,10 @@ class Login:
         self.root.mainloop()
     def New_Open_Window(self):
         self.root = Window("Login")
-        self.root.Add_Field("Username",0,0,75,8,2)
-        self.root.Add_Field("Password",0,1,75,8,2)
+        self.root.Add_Field("Username",0,0,45,8,2)
+        self.root.Add_Field("Password",0,1,45,8,2)
         self.root.Add_Button("Log In",6,0,self.Log_In,10,8,1)
         self.root.Add_Button("Edit User",6,1,self.Edit_Users,10,8,1)
         self.root.Create_Window()
     def Login_Error(self,errortype):
-        # print('Error("Incorrect Information")')
         ErrorPage(errortype)
