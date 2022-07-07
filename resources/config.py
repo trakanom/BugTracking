a95ba45f27758c81a3f2786b67328a114b658329
Trakanom <Mika.Wisener@gmail.com>
2022-07-06 21:54:18 -0700

    add more errors

--- resources/config.py
+++ resources/config.py
@@ -6,5 +6,7 @@ config = {
 
 errors = {
     "Incorrect Information":"Wrong username and/or password. Please try again.",
-    "Unauthorized":"Warning: Insufficient privilege for this operation"
+    "Unauthorized":"Warning: Insufficient privilege for this operation",
+    "Missing Field":"Error: Missing required field(s)",
+    "Another Error":"Debug: remove the catch statement and find out wtf this error is"
 }
