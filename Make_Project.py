ebb5d85803b7daa7c7123bb86346fb8cc7d2297b
Trakanom <Mika.Wisener@gmail.com>
2022-07-07 15:48:43 -0700

    remove incorrect file

--- Make_Project.py
+++
@@ -1,9 +0,0 @@
-Project = {
-	"Author":"Trakanom",
-	"Urgency":"Medium",
-	"Difficulty":"Cursed",
-	"Assignee":"Trakanom",
-	"Date Submitted":"07/07/2022 14:57:00",
-	"Date Due":"Now",
-	"Title":"Make Project",
-	"Description":"Fill out this form then click 'Save' to save it."}
