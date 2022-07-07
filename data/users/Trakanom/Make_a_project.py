6d289519f3cb07f28e9add1c8984c9ed64ce6016
Trakanom <Mika.Wisener@gmail.com>
2022-07-07 15:47:23 -0700

    add project file saved via frontend formatter; yay!

---
+++ data/users/Trakanom/Make_a_project.py
@@ -0,0 +1,9 @@
+Project = {
+	"Author":"Trakanom",
+	"Urgency":"Medium",
+	"Difficulty":"Cursed",
+	"Assignee":"Trakanom",
+	"Date Submitted":"07/07/2022 15:43:38",
+	"Date Due":"Today",
+	"Title":"Make a project",
+	"Description":"Fill out this form, then hit 'Save'"}
