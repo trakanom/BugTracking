028789cbb09972218485e363abebc1a492577479
Trakanom <Mika.Wisener@gmail.com>
2022-07-07 15:21:29 -0700

    give the user the ability to save projects locally

--- resources/classes/Userlists.py
+++ resources/classes/Userlists.py
@@ -1,3 +1,4 @@
+import sys
 class UserList:
     class User_Template:
         def __init__(self, Username, Priv_Level, PW_Hash, First_Name, Last_Name, Email=None, Phone=None):
@@ -8,6 +9,7 @@ class UserList:
             self.Phone = Phone if Phone!=None else "+16508675309"
             self.First_Name = First_Name
             self.Last_Name = Last_Name
+            self.Projects = {}
     def __init__(self):
         self.TheList = {}
     def Add_User(self, Username, Priv_Level, PW_Hash, First_Name, Last_Name, Email=None, Phone=None):
@@ -16,3 +18,16 @@ class UserList:
         pass
     def Delete_User(self):
         pass
+    def Add_Project(self,user,Project):
+        self.TheList[user].Projects[Project["Title"]]=Project
+        #Save project as dict to data\users\{User}\{Project_Title}.py
+        directory = 'data\\users\\'+str(user)+'\\'+str(Project["Title"].replace(" ","_"))+'.py' 
+            
+            
+        r = open(directory, "x")
+        #Form an importable, formatted dict saved as a file for future references.
+        r.write("Project = {")
+        for n,key in enumerate(Project.keys()):
+            string = f"\t\"{key}\":\"{Project[key]}\""
+            r.write((",\n" if n>0 else "\n")+string)
+        r.write("}")
